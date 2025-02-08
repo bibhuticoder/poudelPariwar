b9451362fe8442306e6f8922c448821b4884bca6
Bibhuti Poudyal <bibhutipoudyal@Bibhutis-MacBook-Pro.local>
2025-02-08 23:29:52 +0545

    .

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: false,
-  images: { unoptimized: true },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  }
+  swcMinify: true,
+  images: { unoptimized: true }
 }
 
 module.exports = nextConfig
--- pages/aama-stuti-smriti.tsx
+++ pages/aama-stuti-smriti.tsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react"
+import React from "react"
 import type { NextPage } from "next"
 import Head from "next/head"
-import dynamic from "next/dynamic"
-// const PdfViewer = dynamic(() => import("../components/PdfViewer"), {
-//   ssr: false, // Prevents server-side rendering
-// })
+
 import PdfViewer from '../components/PdfViewer';
-const AamaPage: NextPage = () => {``
+const AamaPage: NextPage = () => {
   return (
     <div className="pt-16">
       <Head>
